1c57c4dd86e0cc0f76e9d6691d2b787c3faee495
Ilias <ildar97.97@gmail.com>
2019-03-29 00:12:14 +0300

    small changes in function index_functionality, deleted imported_modules_by_packages

--- utils.py
+++ utils.py
@@ -3,23 +3,25 @@ from importlib import import_module
 from inspect import getmembers, isfunction
 from json import load
 
+# корневой файл настроек
 with open('settings.json', 'r') as settings_file:
     settings = load(settings_file)
 
+# каталог с исходниками, в котором исходники разбиты по пакетам и модулям
 src = settings["sources"]
 
 # виды API, предоставляемые модулями
 API_types = {'API_functions': isfunction}
 
-# все найденные модули
-imported_modules_by_packages = {}
-
 # API объекты, предоставляемые модулями
-imported_API_by_modules = {}
+# список кортежей вида (полное имя модуля, объект модуля, [ (название объекта API, объект API) ])
+imported_API_by_modules = []
 
 
 def index_functionality():
-    """Перечисление реализованных модулей и API, которое они предоставляют."""
+    """Перечисление реализованных модулей и API, которое они предоставляют.
+    Под пакетами подразумеваются подкаталоги каталога src, а под модулями файлы
+    с исходными кодами в этих подкаталогах."""
 
     # получение имен подкаталогов каталога src
     packages = next(os.walk(src))[1]
@@ -45,22 +47,28 @@ def index_functionality():
         # поиск во всех модулях пакета, предоставляющих API
         for module in pkg_settings["API_modules"]:
 
-            imported_modules_by_packages[f'{pkg}'] = {}
-            imported_modules_by_packages[f'{pkg}'][f'{module}'] = import_module(
-                f'{src}.{pkg}.{module}')
-            imported_API_by_modules[imported_modules_by_packages[f'{pkg}'][f'{module}']] = []
+            # импортирование модуля по его имени
+            module_obj = import_module(f'{src}.{pkg}.{module}')
+
+            # 
+            # imported_API_by_modules[f'{src}.{pkg}.{module}'] = []
+            imported_API_by_modules.append( (f'{src}.{pkg}.{module}', module_obj, []) )
 
             # поиск по типу предоставляемого API
-            for API_type in (set(API_types.keys()) & set(pkg_settings.keys())):
+            for API_type in ( set( API_types.keys() ) & set( pkg_settings.keys() ) ):
 
                 # поиск по всем объектам модуля заданного типа
-                members = getmembers(
-                    imported_modules_by_packages[f'{pkg}'][f'{module}'], API_types[API_type])
+                # members - кортеж вида (название объекта API, объект API)
+                members = getmembers( module_obj, API_types[API_type] )
 
                 # поиск объектов API по допустимым названиям для данного типа API
                 for member in members:
                     for prefix in pkg_settings[API_type]:
 
                         if member[0].startswith(prefix):
-                            imported_API_by_modules[imported_modules_by_packages[f'{pkg}'][f'{module}']].append(member)
+                            imported_API_by_modules[-1][-1].append(member)
                             break
+                
+            # если модуль не содержит объектов API (функции, классы...), то он удаляется
+            if len(imported_API_by_modules[-1][-1]) == 0:
+                imported_API_by_modules.pop()
